98b12b7d2ec4b7bef47909cb4dcc211f4e3775b1
John <jawnnypoo@gmail.com>
2017-11-22 11:02:56 -0600

    Adjustments for API 27

--- app/src/main/java/com/savvyapps/togglebuttonlayout/sample/ToggleButtonLayoutActivity.java
+++ app/src/main/java/com/savvyapps/togglebuttonlayout/sample/ToggleButtonLayoutActivity.java
@@ -5,9 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.savvyapps.togglebuttonlayout.Toggle;
 import com.savvyapps.togglebuttonlayout.ToggleButtonLayout;
@@ -27,14 +25,6 @@ public class ToggleButtonLayoutActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle(R.string.app_name);
-
-        Button buttonSelectMiddle = findViewById(R.id.button_set_middle_selected);
-        buttonSelectMiddle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toggleButtonLayout.setToggled(R.id.toggle_center, true);
-            }
-        });
 
         toggleButtonLayout.setOnToggleSelectedListener(new ToggleButtonLayout.OnToggledListener() {
             @Override
